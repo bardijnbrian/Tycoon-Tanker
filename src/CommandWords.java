6d93fec099bf1aa7c2f74490c851ec8cac73852c
brianB <brianbardijn003@gmail.com>
2024-02-02 20:59:49 +0100

    added quit, next

--- src/CommandWords.java
+++ src/CommandWords.java
@@ -2,7 +2,7 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-
+        "quit", "next"
     };
 
     public CommandWords()
